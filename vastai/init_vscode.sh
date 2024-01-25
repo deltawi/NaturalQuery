2e62711ee78fc252c8999b5052d52fef1695c8e3
Mehdi Tantaoui <mehdi.tantaoui@ibm.com>
2024-01-25 14:36:52 +0300

    removing experiments

--- vastai/init_vscode.sh
+++
@@ -1,3 +0,0 @@
-curl -Lk https://vscode.download.prss.microsoft.com/dbazure/download/stable/0ee08df0cf4527e40edc9aa28f4b5bd38bbff2b2/vscode_cli_alpine_x64_cli.tar.gz --output vscode_cli.tar.gz
-tar -xf vscode_cli.tar.gz
-./code tunnel
