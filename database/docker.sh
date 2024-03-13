f59044a928938192b6e2656a375805ce20716455
deltawi <tantaoui.mehdi@gmail.com>
2024-03-13 12:24:34 +0300

    Update docker.sh

--- database/docker.sh
+++ database/docker.sh
@@ -1,1 +1,1 @@
-docker run -e POSTGRES_PASSWORD=mysecretpassword -p 5432:5432 -d -v ./init_db.sql:/docker-entrypoint-initdb.d/init_db.sql postgres
+docker run -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d -v ./init_db.sql:/docker-entrypoint-initdb.d/init_db.sql postgres
